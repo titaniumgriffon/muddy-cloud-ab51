fda71f1b6047d7a930da6ec6f3ff34c8a49a5936
Luke Mason <lukemason1991@gmail.com>
2025-04-09 22:25:02 -0500

    Spelling

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Mid-West Technology Comittee";
+export const SITE_TITLE = "Mid-West Technology Committee";
 export const SITE_DESCRIPTION = "-";
